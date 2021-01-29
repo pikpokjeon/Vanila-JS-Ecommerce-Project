6e70d418a8a3fc80789a8fddc51045d2b46a06e8
pikpokjeon <pikpokjeon@gmail.com>
2021-01-29 18:37:44 +0900

    api 메인패치 프롭스 넘겨주기

--- src/App.js
+++ src/App.js
@@ -3,28 +3,41 @@ console.log("app is running!");
 class App {
   $target = null;
   data = [];
+  default = 'main'
+  mainData = []
   d = null
 
   constructor($target) {
     this.$target = $target;
 
-    this.header = new Header({$target,data:this.d})
-    this.slidemenu = new SlideMenu({$target,data: this.d})
+    this.header = new Header({ $target, data: this.d })
+    this.slidemenu = new SlideMenu({ $target, data: this.d })
 
-    this.subheader = new SubHeader({$target})
+    this.subheader = new SubHeader({ $target })
 
- 
+
     this.searchInput = new SearchInput({
       $target,
       onSearch: keyword => {
         api.fetchCats(keyword).then(({ data }) => this.setState(data));
       }
     });
-    this.mainlist = new MainList({$target,
-      data : this.data
-  })
 
 
+    const fetchMainData = async () => 
+    {
+      const mainInit = await api.fetchCats('main')
+      this.setMainState(mainInit.data)
+      return mainInit.data
+    }
+
+    this.mainlist = new MainList({
+      $target,
+      // setMainState:  initData => this.mainlist.setMainState(initData),
+      setMainState: (data) => { this.mainData = data },
+      onClick: initData => this.mainlist.setMainState(fetchMainData),
+      initData: fetchMainData(),
+    })
     this.searchResult = new SearchResult({
       $target,
       initialData: this.data,
@@ -50,5 +63,12 @@ class App {
     this.data = nextData;
     this.d = nextData
     this.searchResult.setState(nextData);
+  }
+  setMainState(nextData) {
+    console.log('setMainState', this)
+    this.mainData = nextData
+    console.log('this.mainData', this.mainData)
+
+    this.mainlist.setMainState(nextData)
   }
 }
--- src/MainList.js
+++ src/MainList.js
@@ -1,39 +1,59 @@
 class MainList {
-    $MainList = null;
-    data = null;
-    onClick = null;
-  
-    constructor({ $target, data, onClick }) {
-      this.$MainList = document.createElement("div");
-      this.$MainList.className = "MainList";
-      $target.appendChild(this.$MainList);
-  
-      this.data = data;
-      this.onClick = onClick;
-  
-      this.render();
-    }
-  
-    setState(nextData) {
-      this.data = nextData;
-      this.render();
-    }
-  
-    render() {
-      this.$MainList.innerHTML = this.data
-        .map(
-          cat => `
-            <div class="itembox">
+  $MainList = null;
+  initData = null;
+  onClick = null;
+
+  constructor({ $target, initData, setMainState, onClick }) {
+    this.$MainList = document.createElement("div");
+    this.$MainList.className = "MainList";
+    $target.appendChild(this.$MainList);
+
+    this.initData = initData;
+    this.onClick = onClick;
+    this.setMainState = setMainState;
+    console.log('MainList is created', initData)
+    this.render();
+
+  }
+
+
+  setMainState(nextData) {
+    console.log('set main state :', nextData)
+    this.initData = nextData;
+    this.render();
+  }
+  onClick(data)
+  {
+    this.initData = data
+    this.render()
+  }
+
+  async render() {
+    const a = await this.initData
+    console.log(a)
+    document.addEventListener("click", (e) => {
+    // const data = JSON.parse(this.$target)
+
+      if (a.length > 0) {
+        this.$MainList.innerHTML = a.map(
+            cat => `
+            <div class="itembox2">
               <img src=${cat.url} alt=${cat.name} />
             </div>
           `
-        )
-        .join("");
-  
-      this.$MainList.querySelectorAll(".item").forEach(($item, index) => {
-        $item.addEventListener("click", () => {
-          this.onClick(this.data[index]);
-        });
-      });
-    }
+          )
+          .join("")
+
+        this.$MainList.querySelectorAll(".itembox2").forEach(($item, idx) => {
+          document.addEventListener("click", () => {
+            this.setMainState(a[idx])
+          })
+        })
+
+      }
+
+    })
+    
   }
+}
+
